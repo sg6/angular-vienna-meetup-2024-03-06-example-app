88b4c29afdbb88be931f0256745e1a537688332e
Stefan Georg Rath <stefan@sg6.eu>
2024-03-04 11:48:46 +0100

    network analysis interceptor

--- src/app/interceptors/network-analysis.interceptor.ts
+++ src/app/interceptors/network-analysis.interceptor.ts
@@ -2,11 +2,13 @@ import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpReq
 import { inject } from "@angular/core";
 import { Observable, catchError, throwError } from "rxjs";
 import { NetworkAnalysisService } from "../shared/network-analysis.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 
 export const networkAnalysisInterceptor: HttpInterceptorFn =
     (req: HttpRequest<any>,next: HttpHandlerFn): Observable<HttpEvent<any>> => {
     const networkAnalysisService = inject(NetworkAnalysisService);
+    const modalService = inject(NgbModal);
     
     networkAnalysisService.addHttpCall();
     
@@ -17,7 +19,29 @@ export const networkAnalysisInterceptor: HttpInterceptorFn =
                 successful: false,
                 timestamp: new Date(),
                 error: JSON.stringify(err),
-            })
+            });
+
+            let message = "There seems to be an issue... ";
+            switch (err.status) {
+                case 0: {
+                    message += `The backend is not available.
+                               Please contact the key user.`;
+                    break;
+                }
+                case 401: {
+                    message += `You do not have permissions.
+                               Please log in.`
+                    break;
+                }
+                case 500: {
+                    message += `Internal server error.
+                               Please contact the IT`;
+                    break;
+                }
+            }
+
+            modalService.open(message);
+            
             return throwError(() => new Error('error'));
         })
     );
